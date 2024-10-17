2b55e3584419177912f2260816d12bb914969640
fiqki <fiqkiamrulloh@gmail.com>
2024-10-17 22:17:54 +0700

    added Transportasi as parent class for the Unguided 1 Project

---
+++ PraktikumOOP/Week5P4/Unguided1/TransportVeh/Transportasi.java
@@ -0,0 +1,27 @@
+package PraktikumOOP.Week5P4.Unguided1.TransportVeh;
+
+public abstract class Transportasi {
+    private String nomorKendaraan;
+    private int kapasitas;
+    private String asalTujuan;
+    private double jarak;
+    private String kelasLayanan;
+
+    public Transportasi(String nomorKendaraan, int kapasitas, String asalTujuan, double jarak, String kelasLayanan) {
+        this.nomorKendaraan = nomorKendaraan;
+        this.kapasitas = kapasitas;
+        this.asalTujuan = asalTujuan;
+        this.jarak = jarak;
+        this.kelasLayanan = kelasLayanan;
+    }
+
+    public double getJarak() {
+        return jarak;
+    }
+
+    public String getKelasLayanan() {
+        return kelasLayanan;
+    }
+
+    public abstract double hitungHargaTiket();
+}
