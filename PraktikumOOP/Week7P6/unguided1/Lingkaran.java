8f7e7197d82f244839dfbc798c1400ebc8aedc23
fiqki <fiqkiamrulloh@gmail.com>
2024-11-02 19:25:18 +0700

    class lingkaran as super class

---
+++ PraktikumOOP/Week7P6/unguided1/Lingkaran.java
@@ -0,0 +1,27 @@
+package PraktikumOOP.Week7P6.unguided1;
+
+import java.lang.Math;
+
+public class Lingkaran {
+    private int jarijari;
+
+    public Lingkaran(int jarijari) {
+        this.jarijari = jarijari;
+    }
+
+    public void setJarijari(int jarijari) {
+        this.jarijari = jarijari;
+    }
+
+    public int getJarijari() {
+        return jarijari;
+    }
+
+    public int getLuas() {
+        return (int) Math.PI * (int) Math.pow(jarijari, 2);
+    }
+
+    public int getLuas(int jarijari) {
+        return (int) Math.PI * (int) Math.pow(jarijari, 2);
+    }
+}
