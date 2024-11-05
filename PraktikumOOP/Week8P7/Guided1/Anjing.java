662af3059c01d2c4f60836e5aa707115872cf83d
fiqki <fiqkiamrulloh@gmail.com>
2024-11-05 14:17:55 +0700

    class anjing as extend class

---
+++ PraktikumOOP/Week8P7/Guided1/Anjing.java
@@ -0,0 +1,12 @@
+package PraktikumOOP.Week8P7.Guided1;
+
+public class Anjing extends Hewan{
+    @Override
+    void suara() {
+        System.out.println("Bark...");
+    }
+
+    void tidur() {
+        System.out.println("Anjing tertidur");
+    }
+}
