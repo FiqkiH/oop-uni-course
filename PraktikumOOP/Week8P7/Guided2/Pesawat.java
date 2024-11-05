b409a4902c3f94325a5947ed1d820e590fbfc1f9
fiqki <fiqkiamrulloh@gmail.com>
2024-11-05 14:18:58 +0700

    class Pesawat  as extend class

---
+++ PraktikumOOP/Week8P7/Guided2/Pesawat.java
@@ -0,0 +1,8 @@
+package PraktikumOOP.Week8P7.Guided2;
+
+public class Pesawat implements BisaTerbang{
+    @Override
+    public void bisaTerbang() {
+        System.out.println("Pesawat bisa terbang");
+    }
+}
