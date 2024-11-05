5b82ba435643555b5fd97ce3b09bc111ac7ac6b5
fiqki <fiqkiamrulloh@gmail.com>
2024-11-05 14:19:41 +0700

    added demo and compile generated files

---
+++ PraktikumOOP/Week8P7/Guided2/DemoInterface.java
@@ -0,0 +1,12 @@
+package PraktikumOOP.Week8P7.Guided2;
+
+public class DemoInterface {
+    public static void main(String[] args) {
+        BisaTerbang burung = new Burung();
+        BisaTerbang pesawat = new Pesawat();
+
+        burung.bisaTerbang();
+        pesawat.bisaTerbang();
+    }
+    
+}
