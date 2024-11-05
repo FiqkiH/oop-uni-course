6f43b028fc8e75bf85065f8190daccb71abf2583
fiqki <fiqkiamrulloh@gmail.com>
2024-11-05 14:17:29 +0700

    class hewan as abstract class

---
+++ PraktikumOOP/Week8P7/Guided1/Hewan.java
@@ -0,0 +1,9 @@
+package PraktikumOOP.Week8P7.Guided1;
+
+abstract class Hewan {
+    abstract void suara(); //metode abstrak, tidak memiliki isi
+
+    void tidur() {
+        System.out.println("Hewan sedang tidur");
+    }
+}
