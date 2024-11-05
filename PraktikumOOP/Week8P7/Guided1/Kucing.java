d1c30762ff10effc47d908b4dfaf0004f47b89be
fiqki <fiqkiamrulloh@gmail.com>
2024-11-05 14:17:46 +0700

    class kucing as extend class

---
+++ PraktikumOOP/Week8P7/Guided1/Kucing.java
@@ -0,0 +1,12 @@
+package PraktikumOOP.Week8P7.Guided1;
+
+public class Kucing extends Hewan {
+    @Override
+    void suara() {
+        System.out.println("Meow...");
+    }
+
+    void tidur() {
+        System.out.println("Kucing tertidur");
+    }
+}
