9fe1b0a467346885288daa91a68a8c14e0923373
fiqki <fiqkiamrulloh@gmail.com>
2024-11-05 14:18:47 +0700

    class Burung as extend class

---
+++ PraktikumOOP/Week8P7/Guided2/Burung.java
@@ -0,0 +1,8 @@
+package PraktikumOOP.Week8P7.Guided2;
+
+public class Burung implements BisaTerbang{
+    @Override
+    public void bisaTerbang() {
+        System.out.println("Burung bisa terbang");
+    }
+}
