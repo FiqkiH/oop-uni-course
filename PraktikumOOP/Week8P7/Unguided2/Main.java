e4d3d173c6fb9bbd7172a428533a21b3b0963bbb
fiqki <fiqkiamrulloh@gmail.com>
2024-11-11 19:20:42 +0700

    added main class and compile generated class files

---
+++ PraktikumOOP/Week8P7/Unguided2/Main.java
@@ -0,0 +1,12 @@
+package PraktikumOOP.Week8P7.Unguided2;
+
+public class Main {
+    public static void main(String[] args) {
+        Menggambar lingkaran = new Lingkaran();
+        Menggambar persegi = new Persegi();
+
+        lingkaran.menggambar();
+        persegi.menggambar();
+    }
+    
+}
