09401f0928e47dd3efaf8f6281fa18265d864e69
fiqki <fiqkiamrulloh@gmail.com>
2024-11-11 18:37:43 +0700

    implement abstact class for instrument sound

---
+++ PraktikumOOP/Week8P7/Unguided1/Gitar.java
@@ -0,0 +1,8 @@
+package PraktikumOOP.Week8P7.Unguided1;
+
+public class Gitar extends AlatMusik {
+    @Override
+    public void bunyi() {
+        System.out.println("Gitar menggenjreng");
+    }
+}
---
+++ PraktikumOOP/Week8P7/Unguided1/Piano.java
@@ -0,0 +1,8 @@
+package PraktikumOOP.Week8P7.Unguided1;
+
+public class Piano extends AlatMusik{
+    @Override
+    public void bunyi() {
+        System.out.println("Piano tulalit");
+    }
+}
