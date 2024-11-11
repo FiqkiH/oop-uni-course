3b3c823ea98ff13828d7c917808d0f7e7cbe391c
fiqki <fiqkiamrulloh@gmail.com>
2024-11-11 18:38:08 +0700

    create main method to test and added generated compiled class files

---
+++ PraktikumOOP/Week8P7/Unguided1/Main.java
@@ -0,0 +1,12 @@
+package PraktikumOOP.Week8P7.Unguided1;
+
+public class Main {
+    public static void main(String[] args) {
+        AlatMusik gitar = new Gitar();
+        AlatMusik piano = new Piano();
+
+        gitar.bunyi();
+        piano.bunyi();
+    }
+    
+}
