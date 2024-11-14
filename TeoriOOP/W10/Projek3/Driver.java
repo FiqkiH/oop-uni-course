73b1c94fc5c3992cb183855b579082cdfd7638c2
fiqki <fiqkiamrulloh@gmail.com>
2024-11-14 08:34:23 +0700

    testing using only static method

---
+++ TeoriOOP/W10/Projek3/Driver.java
@@ -0,0 +1,15 @@
+package TeoriOOP.W10.Projek3;
+
+public class Driver {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                CounterMachine.count();
+            }
+        }
+
+        System.out.println(
+            CounterMachine.getCounter()
+        );
+    }
+}
