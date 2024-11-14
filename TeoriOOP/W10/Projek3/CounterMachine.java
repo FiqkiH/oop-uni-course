c5a94fd706903ef0236a32d045d4547a98511089
fiqki <fiqkiamrulloh@gmail.com>
2024-11-14 08:36:02 +0700

    using static method and static variable

--- TeoriOOP/W10/Projek3/CounterMachine.java
+++ TeoriOOP/W10/Projek3/CounterMachine.java
@@ -1,13 +1,9 @@
 package TeoriOOP.W10.Projek3;
 
 public class CounterMachine {
-    private static int counter = 0;
+    public static int counter = 0;
 
     public static void count() {
         counter++;
-    }
-
-    public static int getCounter() {
-        return counter;
     }
 }
