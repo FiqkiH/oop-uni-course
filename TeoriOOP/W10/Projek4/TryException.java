137a245d9080fb2c5b5f523deb40b0ad712e7c59
fiqki <fiqkiamrulloh@gmail.com>
2024-11-15 01:53:58 +0700

    added class for accepting int number

---
+++ TeoriOOP/W10/Projek4/TryException.java
@@ -0,0 +1,13 @@
+package TeoriOOP.W10.Projek4;
+
+public class TryException {
+    public int number;
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
