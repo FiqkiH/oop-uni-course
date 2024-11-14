00484b72692a7ca63f0320452d5cef763be6f769
fiqki <fiqkiamrulloh@gmail.com>
2024-11-14 08:11:22 +0700

    modify to use static variable, and it will return 4 on the Driver class

--- TeoriOOP/W10/Projek1/Duck.java
+++ TeoriOOP/W10/Projek1/Duck.java
@@ -2,7 +2,7 @@ package TeoriOOP.W10.Projek1;
 
 public class Duck {
     private int size;
-    private int duckCount = 0;
+    private static int duckCount = 0;
 
     public Duck(int size) {
         this.size = size;
