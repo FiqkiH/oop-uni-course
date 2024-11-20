9d3c3a41d4dc4e4762b7bcea1610762772183fd2
fiqki <fiqkiamrulloh@gmail.com>
2024-11-20 10:25:33 +0700

    learning overriding method from String

---
+++ PraktikumOOP/Week10/Guided3/Person.java
@@ -0,0 +1,24 @@
+package PraktikumOOP.Week10.Guided3;
+
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + age + " y.o)";
+    }
+}
