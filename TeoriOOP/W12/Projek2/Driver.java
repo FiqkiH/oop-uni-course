25486ffdf8e25a642500f409e585c90255439d55
fiqki <fiqkiamrulloh@gmail.com>
2024-11-21 08:17:38 +0700

    test for TryException inside class

--- TeoriOOP/W12/Projek2/Driver.java
+++ TeoriOOP/W12/Projek2/Driver.java
@@ -4,12 +4,8 @@ public class Driver {
     public static void main(String[] args) {
         TryException t = new TryException();
 
-        try {
-            t.setNumber(8, 10);
-            System.out.println(t.getNumber(4));
-        } catch (Exception e) {
-            System.out.println("Exception occurs");
-        }
+        t.setNumber(8, 10);
+        System.out.println(t.getNumber(4));
     }
     
 }
