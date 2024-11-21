081ba282f374de8d4d614d1fa6a1a4c5e4dd7e12
fiqki <fiqkiamrulloh@gmail.com>
2024-11-21 08:56:46 +0700

    added user class

---
+++ TeoriOOP/W12/Projek4/User.java
@@ -0,0 +1,18 @@
+package TeoriOOP.W12.Projek4;
+
+public class User {
+    private String id, name;
+
+    public User(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
