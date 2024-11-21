9914c9e37bd835ff5534c799e0a32836030e9880
fiqki <fiqkiamrulloh@gmail.com>
2024-11-21 08:19:56 +0700

    trying try exception outside class

---
+++ TeoriOOP/W12/Projek1/People.java
@@ -0,0 +1,13 @@
+package TeoriOOP.W12.Projek1;
+
+public class People {
+    public String name;
+
+    public People(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
