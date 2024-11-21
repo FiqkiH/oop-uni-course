8b7eb1ccf3b9eefadd2fa6acf564753cfc0f9741
fiqki <fiqkiamrulloh@gmail.com>
2024-11-21 08:14:50 +0700

    TryException outside class

---
+++ TeoriOOP/W12/Projek2/TryException.java
@@ -0,0 +1,13 @@
+package TeoriOOP.W12.Projek2;
+
+public class TryException {
+    int number[] = new int[5];
+
+    public void setNumber(int id, int number) {
+        this.number[id] = 5/number;
+    }
+    public int getNumber(int id) {
+        return number[id];
+    }
+    
+}
