a1e5829c04594035cd5f2c344218e77c6372d6e7
fiqki <fiqkiamrulloh@gmail.com>
2024-11-23 18:27:57 +0700

    add counter on constructor, override toString method

--- PraktikumOOP/Week10/Unguided1/Cat.java
+++ PraktikumOOP/Week10/Unguided1/Cat.java
@@ -6,6 +6,7 @@ public class Cat extends Animal implements Behaviour{
     public Cat(String nama, int umur, String breed) {
         super(nama, umur);
         this.breed = breed;
+        Animal.hitungJumlahHewan();
     }
 
     public void meow() {
@@ -15,5 +16,10 @@ public class Cat extends Animal implements Behaviour{
     @Override
     public void describeBehaviour() {
         System.out.println("Kucing suka bermain dan memanjat pohon");
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{Nama: " + getNama() + " Umur: " + getUmur() + " Breed: " + breed +"}";
     }
 }
